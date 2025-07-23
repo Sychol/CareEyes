cc56b32ea92714f8a47ff942a2c9d7b34b42d86e
WoH3D <j200chi@naver.com>
2025-07-23 15:56:04 +0900

    mybatis 의존성 통합용

---
+++ backend/src/main/java/com/careeyes/entity/Event.java
@@ -0,0 +1,13 @@
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Event {
+
+    private int event_id;
+    private Timestamp event_time;
+    private int cctv_id;
+    private String img_path;
+    private int manage;
+    
+}
--- backend/src/main/java/com/careeyes/entity/Member.java
+++ backend/src/main/java/com/careeyes/entity/Member.java
@@ -10,4 +10,16 @@ import lombok.NoArgsConstructor;
 public class Member {
 
 	private String member_id;
+	private String password;
+	private String name;
+	private String email;
+	private String phone;
+	private Role role;
+	private String company;
+	private String department;
+
+}
+
+enum Role {
+	ADMIN, MEMBER; // enum은 상수로 간주되므로 대문자 권장
 }
