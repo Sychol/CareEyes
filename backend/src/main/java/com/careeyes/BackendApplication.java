b6ea68c3cd6d34411a44b531e9e6cb9b03ac7702
WoH3D <j200chi@naver.com>
2025-08-08 14:41:46 +0900

    타임존 한국으로 설정

--- backend/src/main/java/com/careeyes/BackendApplication.java
+++ backend/src/main/java/com/careeyes/BackendApplication.java
@@ -1,5 +1,8 @@
 package com.careeyes;
 
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,7 +11,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendApplication {
 
+
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("현재 시간은??" + LocalDateTime.now());
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
